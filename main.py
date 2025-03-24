9470a957f032febe10e26446e59ea23c6d7e7e49
Leonardo <LeonardoChe888@ya.ru>
2025-03-24 19:46:25 +0700

    FINISH

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-from datetime import datetime
-
+from flask import Flask, render_template
 from data import db_session
 from data.db_session import global_init, create_session
+from data.jobs import Jobs
 
+
+app = Flask(__name__)
 db_session.global_init("db/blogs.db")
-import datetime
-from collections import defaultdict
+db_sees = db_session.create_session()
+data = []
+def is_finished(f):
+    if f:
+        return "is finished"
+    else:
+        return "is not finished"
 
+for i in db_sees.query(Jobs).all():
+    data.append([i.id, i.job, i.team_leader, i.work_size, i.collaborators, is_finished(i.is_finished)])
 
-name = input()
+@app.route("/")
+def start():
+    return render_template("index.html", data=data)
 
-
-global_init(name)
-db_sess = create_session()
-
-for i in db_sess.query(User).filter(User.address == "module_1", User.age < 21).all():
-    i.address = "module_3"
-    db_sess.commit()
+if __name__ == "__main__":
+    app.run(port=8080, host="127.0.0.1")
